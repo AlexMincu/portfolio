55ee1d3a372c8c1ba62f514721d2a3c25ed86afb
Alex <minculescualex98@gmail.com>
2023-07-20 23:49:25 +0300

    init project + tailwindcss and prettier plugin

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>alMinculescu | Portfolio</title>
+	<meta name="description" content="Alex Minculescu Portfolio website" />
+</svelte:head>
+
+<section>
+	<h1 class="h1 m-6 bg-red-500 text-center text-4xl text-white">Hello World!</h1>
+</section>
