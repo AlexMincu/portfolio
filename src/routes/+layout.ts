74ccc2545e38db5bec42ad3c50745557250b750d
Alex <minculescualex98@gmail.com>
2023-08-10 10:44:40 +0300

    vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,9 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+// Vercel analytics
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
